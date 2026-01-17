a377357726a20278fa575b8a871f2df8263c3f00
KettleOfKetchup <kettleofketchup@gmail.com>
2026-01-17 14:10:39 -0500

    feat: add OrganizationCard component

--- frontend/app/components/organization/index.ts
+++ frontend/app/components/organization/index.ts
@@ -2,3 +2,4 @@ export { OrganizationSchema, CreateOrganizationSchema } from './schemas';
 export type { OrganizationType, OrganizationsType, CreateOrganizationInput } from './schemas';
 export { useOrganizations } from './hooks/useOrganizations';
 export { useOrganization } from './hooks/useOrganization';
+export { OrganizationCard } from './OrganizationCard';
